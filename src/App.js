50c6885cf727de0b7ec15510f2bf69fde06bd096
alexaMRR <44932717+alexaMRR@users.noreply.github.com>
2021-12-16 17:48:43 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,142 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+let BobTheBuilder = {
+  name: "Bob the Builder",
+  school: "School of Hard Hats",
+  year: "2001",
+  color: "Yellow",
+  music: "Nursery Rhymes",
+  url: "https://www.youtube.com/watch?v=kXj_1u7YlUY",
+  socialMedia: "TikTok",
+  beachOrMountain: "Mountains"
+};
+
+let Karin = {
+  name: "Karin",
+  school: "UC Berkeley",
+  year: "2017",
+  color: "Sage",
+  music: "Country",
+  url: "https://www.youtube.com/watch?v=oOlDewpCfZQ",
+  socialMedia: "Instagram",
+  beachOrMountain: "Beach"
+};
+
+let Ana = {
+  name: "Ana",
+  school: "CSUEB",
+  year: "2016",
+  color: "Green",
+  music: "Reggea",
+  url: "https://www.youtube.com/channel/UCKxbp_TrC0liu7tl0xsmdoA",
+  socialMedia: "Instagram",
+  beachOrMountain: "Beach"
+};
+
+let Joe = {
+  name: "Joe",
+  school: "School of Schools",
+  year: "2021",
+  color: "Orange",
+  music: "Music",
+  url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  socialMedia: "",
+  beachOrMountain: ""
+};
+
+let Jayden = {
+  name: "Jayden",
+  school: "Burton High School",
+  year: "2023",
+  color: "Blue",
+  music: "No Prefrence",
+  url: "",
+  socialMedia: "Instagram",
+  beachOrMountain: "Beach"
+};
+
+let Uber = {
+  name: "uber",
+  school: "lowella",
+  year: "1940",
+  color: "burger",
+  music: "classic",
+  url: "",
+  socialMedia: "",
+  beachOrMountain: ""
+};
+
+let Maya = {
+  name: "Maya",
+  school: "UC San Diego",
+  year: "2016",
+  color: "Purple",
+  music: "Dance pop",
+  url: "https://www.youtube.com/c/Taskmaster",
+  socialMedia: "Instagram",
+  beachOrMountain: "Mountains"
+};
+
+let David = {
+  name: "David",
+  school: "Burton",
+  year: "2023",
+  color: "Crimson Red",
+  music: "Folk",
+  url: "https://www.youtube.com/watch?v=vWJvQveHDLk",
+  socialMedia: "",
+  beachOrMountain: ""
+};
+
+let Kevin = {
+  name: "Kevin",
+  school: "Carnegie Mellon",
+  year: "2015",
+  color: "Blue",
+  music: "Pop",
+  url: "https://www.youtube.com/c/twosetviolin",
+  socialMedia: "Instagram",
+  beachOrMountain: "Beach"
+};
+
+let Old = {
+  name: "Old",
+  school: "School",
+  year: "1987",
+  color: "Red",
+  music: "Rock",
+  url: "",
+  socialMedia: "",
+  beachOrMountain: "Mountains"
+};
+
+let Ca = {
+  name: "ca",
+  school: "bal",
+  year: "2022",
+  color: "blue",
+  music: "pop",
+  url: "N/A",
+  socialMedia: "N/A",
+  beachOrMountain: ""
+};
+
+let MikeWazowski = {
+  name: "Mike Wazowski",
+  school: "Monster University",
+  year: "5021",
+  color: "lime green",
+  music: "Kpop",
+  url: "https://www.youtube.com/watch?v=QllBbRPTsE0",
+  socialMedia: "TikTok",
+  beachOrMountain: "Mountains"
+};
